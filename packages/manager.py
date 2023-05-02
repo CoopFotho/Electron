853147f639f2f5768e026d6a699eba21cacedc1a
Coop <83347856+CoopFotho@users.noreply.github.com>
2023-05-02 14:13:54 +1000

    Update manager.py

--- packages/manager.py
+++ packages/manager.py
@@ -1,1 +1,10 @@
+# This is used with evel() in python.
+# At the end of each URL is "|" which separates URL and FileName.
+# Make sure there are no spaces anywhere.
 
+{
+ "test1": "|test1",
+ "test2": "|test2",
+ "g++": "https://github.com/CoopFotho/Projects/raw/main/Auto%20Clicker/Auto%20Clicker%20v1.0.exe|g++.exe",
+ "cpp": "https://github.com/CoopFotho/Projects/raw/main/Auto%20Clicker/Auto%20Clicker%20v1.0.exe|cpp.exe"
+}
