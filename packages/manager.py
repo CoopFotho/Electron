764093774c1d981c14b81fe1ec822aa80ba87b5d
Coop <83347856+CoopFotho@users.noreply.github.com>
2023-05-02 14:29:54 +1000

    Update manager.py

--- packages/manager.py
+++ packages/manager.py
@@ -5,7 +5,6 @@
 {
  "test1": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test1",
  "test2": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test2",
- "test3": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test3",
- "g++": "https://github.com/CoopFotho/Projects/raw/main/Auto%20Clicker/Auto%20Clicker%20v1.0.exe|g++.exe",
+ "test3": "https://github.com/CoopFotho/Projects/raw/main/Auto%20Clicker/Auto%20Clicker%20v1.0.exe|test3",
  "cpp": "https://github.com/CoopFotho/Projects/raw/main/Auto%20Clicker/Auto%20Clicker%20v1.0.exe|cpp.exe"
 }
