2fca9d1ef3c6e8e1bbd6e945257fbc03d9b42d04
Coop <83347856+CoopFotho@users.noreply.github.com>
2023-05-03 15:48:35 +1000

    Create package_manager.py

---
+++ package_manager.py
@@ -0,0 +1,8 @@
+# This is used with evel() in python.
+# At the end of each URL is "|" which separates URL and FileName.
+# Make sure there are no spaces anywhere.
+
+{
+ "test1": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test1.exe",
+ "test2": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test2.exe",
+}
