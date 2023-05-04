df73671152bb6d2a92d2218b12b51017992278e7
Coop <83347856+CoopFotho@users.noreply.github.com>
2023-05-04 22:33:41 +1000

    Update package_manager.py

--- package_manager.py
+++ package_manager.py
@@ -1,6 +1,7 @@
 # At the end of each URL is "|" which separates URL and FileName.
+# URL|FolderName|FileName|Version
 {
  "test1": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test1.exe",
  "test2": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test2.exe",
- "beta|v1.0.0": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test|test.exe",
+ "beta": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test|test.exe|v1.0.0",
 }
