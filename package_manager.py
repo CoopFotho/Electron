c8d886779dd120848ecd059c57824b7e41e69b85
Coop <83347856+CoopFotho@users.noreply.github.com>
2023-05-05 20:23:35 +1000

    Update package_manager.py

--- package_manager.py
+++ package_manager.py
@@ -3,5 +3,5 @@
  "test1": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test1.exe",
  "test2": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test2.exe",
  "beta": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|beta|test.exe|v0.0.1",
- "zip": "https://github.com/CoopFotho/Electron/raw/main/packages/test.zip|zip|zip.zip|v0.0.1"
+ "zip": "https://github.com/CoopFotho/Electron/raw/main/packages/zip.zip|zip|zip.zip|v0.0.1"
 }
