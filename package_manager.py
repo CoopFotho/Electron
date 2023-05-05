0b210bea1ba8735d129b676d44bc3ad56c107143
Coop <83347856+CoopFotho@users.noreply.github.com>
2023-05-05 20:07:25 +1000

    Update package_manager.py

--- package_manager.py
+++ package_manager.py
@@ -1,5 +1,4 @@
-# At the end of each URL is "|" which separates URL and FileName.
-# URL|FolderName|FileName|Version
+# URL|FolderName|FileName|Version (No Spaces)
 {
  "test1": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test1.exe",
  "test2": "https://github.com/CoopFotho/Electron/raw/main/packages/test.exe|test2.exe",
